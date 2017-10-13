5e1abd7ee5639e11b824443d409f0d83f2888353
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-10-12 17:55:42 -0700

    Added /permissive- test for headerS

---
+++ math3/permissive.cpp
@@ -0,0 +1,8 @@
+// This file has /permissive- when building with VS 2017
+
+#pragma warning(disable : 4514 4668) // Disable "off by default" warnings when bulding with /Wall
+
+#include <directxmath.h>
+#include <directxcolors.h>
+#include <directxpackedvector.h>
+#include <directxcollision.h>
