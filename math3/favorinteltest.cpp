214c56b10cae1a859e41a6e0b80c612b89558676
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-03-04 15:38:28 -0800

    Prefer use of shuffle to permute for non-Intel processors

---
+++ math3/favorinteltest.cpp
@@ -0,0 +1,25 @@
+//-------------------------------------------------------------------------------------
+// favorintel.cpp - DirectXMath Test Suite
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// http://go.microsoft.com/fwlink/?LinkID=615560
+//-------------------------------------------------------------------------------------
+
+// This test file validates the optional _XM_FAVOR_INTEL_ define
+
+// Off by default warnings
+#pragma warning(disable : 4514 4668 4820)
+// C4514 'function' : unreferenced inline function has been removed 
+// C4668 not defined as a preprocessor macro
+// C4820 padding added after data member
+
+#pragma warning(disable: 26812)
+// 26812: Prefer 'enum class' over 'enum' (Enum.3).
+
+#define _XM_FAVOR_INTEL_
+#include <directxmath.h>
+#include <directxcolors.h>
+#include <directxpackedvector.h>
+#include <directxcollision.h>
