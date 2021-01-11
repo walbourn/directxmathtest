291981218864ef585126b60cfc111c47f1a0d61b
Chuck Walbourn <chuckw@windows.microsoft.com>
2021-01-10 20:43:52 -0800

    Added header test

---
+++ headertest/main.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This test ensures that all public headers fully include all their dependancies, as well compile cleanly at maximum warning level
+
+#include "DirectXMath.h"
+#include "DirectXColors.h"
+#include "DirectXPackedVector.h"
+#include "DirectXCollision.h"
+
+int main()
+{
+    return 0;
+}
