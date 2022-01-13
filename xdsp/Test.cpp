0c81bb20bc5546741db4a3570f67dff01f459d5b
Chuck Walbourn <chuckw@windows.microsoft.com>
2022-01-12 17:14:59 -0800

    modified the XDSP test epsilon (#9)

--- xdsp/Test.cpp
+++ xdsp/Test.cpp
@@ -36,7 +36,7 @@
 typedef DirectX::XMVECTOR XVECTOR;
 typedef DirectX::XMVECTORF32 XVEC;
 
-#define TESTEPSILON  0.00001f
+#define TESTEPSILON  0.0000124f
 
 inline bool Compare(float a, float b)
 {
