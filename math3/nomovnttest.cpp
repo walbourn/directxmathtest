645c866e6e35d4b4b32decd7b2f4af5373df036a
Chuck Walbourn <chuckw@microsoft.com>
2022-12-05 00:44:08 -0800

    Fix warnings with VS 2022 Update 4

--- math3/nomovnttest.cpp
+++ math3/nomovnttest.cpp
@@ -10,10 +10,12 @@
 // This test file validates the optional _XM_NO_MOVNT_ define
 
 // Off by default warnings
-#pragma warning(disable : 4514 4668 4820)
+#pragma warning(disable : 4619 4616 4514 4668 4820 5264)
+// C4619/4616 #pragma warning warnings
 // C4514 'function' : unreferenced inline function has been removed 
 // C4668 not defined as a preprocessor macro
 // C4820 padding added after data member
+// C5264 'const' variable is not used
 
 #pragma warning(disable: 26812)
 // 26812: Prefer 'enum class' over 'enum' (Enum.3).
