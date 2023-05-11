a2ec40d0c38a0c75c34ab7051f44c35439da6607
Chuck Walbourn <chuckw@microsoft.com>
2023-05-11 09:55:52 -0700

    Conformance fix for test

--- xdsp/Test.cpp
+++ xdsp/Test.cpp
@@ -107,8 +107,8 @@ bool FFT4Test()
 
     FFT4( reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), 1 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = { { { { 10.f, -4.f, -2.f, 0.f } } } };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = { { { { 26.f, 0.f, -2.f, -4.f } } } };
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = { { { { 10.f, -4.f, -2.f, 0.f } } } };
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = { { { { 26.f, 0.f, -2.f, -4.f } } } };
 
     if ( !Compare( rinput, ranswer, 1 ) )
     {
@@ -138,8 +138,8 @@ bool FFT8Test()
 
     FFT8(reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), 1 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = { { { { 36.f, -13.656855f, -8.f, -5.656854f } } }, { { { -4.f, -2.343146f, 0.f, 5.656854f } } } };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = { { { { 100.f, 5.656854f, 0.f, -2.343146f } } }, { { { -4.f, -5.656854f, -8.f, -13.656855f } } } };
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = { { { { 36.f, -13.656855f, -8.f, -5.656854f } } }, { { { -4.f, -2.343146f, 0.f, 5.656854f } } } };
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = { { { { 100.f, 5.656854f, 0.f, -2.343146f } } }, { { { -4.f, -5.656854f, -8.f, -13.656855f } } } };
     if ( !Compare( rinput, ranswer, 2 ) )
     {
         printf("ERROR: Failed comparison FFT8 real\n" );
@@ -168,13 +168,13 @@ bool FFT16Test()
 
     FFT16( reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), 1 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = {
         { { { 136.f, -16.f, -8.f, 0.f } } },
         { { { -48.218716f, -13.345429f, -6.408703f, 3.972846f } } },
         { { { -27.313709f, -11.313709f, -4.686292f, 11.313707f } } },
         { { { -19.972847f, -9.591298f, -2.654571f, 32.218716f } } }
     };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = {
         { { { 137.600006f, 0.f, -8.f, -16.f } } },
         { { { 32.218716f, -2.654571f, -9.591298f, -19.972847f } } },
         { { { 11.313708f, -4.686292f, -11.313708f, -27.313709f } } },
@@ -213,13 +213,13 @@ bool FFTTest()
 
     FFT(reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), reinterpret_cast<XMVECTOR*>(unity.get()), 32, 1 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = {
         { { { 273.599976f, -54.627419f, -32.f, -22.627415f } } }, { { { -15.999985f, -9.372581f, 0.f, 22.627415f } } },
         { { { -1.115318f, -0.287085f, -0.182067f, -0.130334f } } }, { { { -0.090151f, -0.046550f, 0.021848f, 0.229655f } } },
         { { { -96.437424f, -39.945694f, -26.690855f, -19.182598f } } }, { { { -12.817410f, -5.309145f, 7.945688f, 64.437424f } } },
         { { { -0.429656f, -0.221848f, -0.153450f, -0.109849f } } }, { { { -0.069667f, -0.017935f, 0.087085f, 0.915322f } } }
     };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = {
         { { { 280.f, 22.627415f, -0.000008f, -9.372585f } } }, { { { -16.f, -22.627415f, -31.999992f, -54.627415f } } },
         { { { 0.915318f, 0.087087f, -0.017932f, -0.069664f } } }, { { { -0.109847f, -0.153451f, -0.221851f, -0.429657f } } },
         { { { 64.437431f, 7.945694f, -5.309141f, -12.817406f } } }, { { { -19.182600f, -26.690861f, -39.945694f, -96.437431f } } },
@@ -248,13 +248,13 @@ bool FFTUnswizzleTest()
 {
     using namespace XDSP;
 
-    static const XM_ALIGNED_DATA(16) XVEC input[] = { { { { 0.f, 1.f, 2.f, 3.f } } },{ { { 4.f, 5.f, 6.f, 7.f } } },{ { { 8.f , 9.f, 10.f, 11.f } } },{ { { 12.f, 13.f, 14.f, 15.f } } } };
+    XM_ALIGNED_DATA(16) static const XVEC input[] = { { { { 0.f, 1.f, 2.f, 3.f } } },{ { { 4.f, 5.f, 6.f, 7.f } } },{ { { 8.f , 9.f, 10.f, 11.f } } },{ { { 12.f, 13.f, 14.f, 15.f } } } };
 
     ScopedAlignedArrayXVEC output(reinterpret_cast<XVEC*>(_aligned_malloc(sizeof(XVEC) * 4, 16)));
 
     FFTUnswizzle(reinterpret_cast<XMVECTOR*>(output.get()), reinterpret_cast<const XMVECTOR*>(input), 4);
 
-    static const XM_ALIGNED_DATA(16) XVEC answer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC answer[] = {
         { { { 0.f, 4.f, 8.f, 12.f } } },
         { { { 1.f, 5.f, 9.f, 13.f } } },
         { { { 2.f, 6.f, 10.f, 14.f } } },
@@ -285,7 +285,7 @@ bool FFTPolarTest()
 
     FFTPolar(reinterpret_cast<XMVECTOR*>(output.get()), reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), 32 );
 
-    static const XM_ALIGNED_DATA(16) XVEC answer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC answer[] = {
         { { { 0.097628f, 0.185826f, 0.274146f, 0.362500f } } }, { { { 0.450867f, 0.539241f, 0.627620f, 0.716000f } } },
         { { { 0.804383f, 0.892766f, 0.981150f, 1.069535f } } }, { { { 1.157921f, 1.246307f, 1.334693f, 1.423080f } } },
         { { { 0.106434f, 0.194655f, 0.282981f, 0.371336f } } }, { { { 0.459704f, 0.548079f, 0.636457f, 0.724838f } } },
@@ -307,14 +307,14 @@ bool DeinterleaveTest()
 {
     using namespace XDSP;
 
-    static const XM_ALIGNED_DATA(16) XVEC input[] = { { { { 1.f, 2.f, 3.f, 4.f } } }, { { { 5.f, 6.f, 7.f, 8.f } } }, { { { 9.f, 10.f, 11.f, 12.f } } }, { { { 13.f, 14.f, 15.f, 16.f } } },
+    XM_ALIGNED_DATA(16) static const XVEC input[] = { { { { 1.f, 2.f, 3.f, 4.f } } }, { { { 5.f, 6.f, 7.f, 8.f } } }, { { { 9.f, 10.f, 11.f, 12.f } } }, { { { 13.f, 14.f, 15.f, 16.f } } },
                                   { { { 1.1f, 2.1f, 3.1f, 4.1f } } }, { { { 5.1f, 6.1f, 7.1f, 8.1f } } }, { { { 9.1f, 10.1f, 11.1f, 12.1f } } }, { { { 13.1f, 14.1f, 15.1f, 16.1f } } } };
 
     ScopedAlignedArrayXVEC output(reinterpret_cast<XVEC*>(_aligned_malloc(sizeof(XVEC) * 8, 16)));
 
     Deinterleave(reinterpret_cast<XMVECTOR*>(output.get()), reinterpret_cast<const XMVECTOR*>(input), 2, 16 );
 
-    static const XM_ALIGNED_DATA(16) XVEC answer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC answer[] = {
         { { { 1.f, 3.f, 5.f, 7.f } } }, { { { 9.f, 11.f, 13.f, 15.f } } }, { { { 1.1f, 3.1f, 5.1f, 7.1f } } }, { { { 9.1f, 11.1f, 13.1f, 15.1f } } },
         { { { 2.f, 4.f, 6.f, 8.f } } }, { { { 10.f, 12.f, 14.f, 16.f } } }, { { { 2.1f, 4.1f, 6.1f, 8.1f } } }, { { { 10.1f, 12.1f, 14.1f, 16.1f } } }
     };
@@ -334,14 +334,14 @@ bool InterleaveTest()
 {
     using namespace XDSP;
 
-    static const XM_ALIGNED_DATA(16) XVEC input[] = { { { { 1.f, 2.f, 3.f, 4.f } } }, { { { 5.f, 6.f, 7.f, 8.f } } }, { { { 9.f, 10.f, 11.f, 12.f } } }, { { { 13.f, 14.f, 15.f, 16.f } } },
+    XM_ALIGNED_DATA(16) static const XVEC input[] = { { { { 1.f, 2.f, 3.f, 4.f } } }, { { { 5.f, 6.f, 7.f, 8.f } } }, { { { 9.f, 10.f, 11.f, 12.f } } }, { { { 13.f, 14.f, 15.f, 16.f } } },
                                   { { { 1.1f, 2.1f, 3.1f, 4.1f } } }, { { { 5.1f, 6.1f, 7.1f, 8.1f } } }, { { { 9.1f, 10.1f, 11.1f, 12.1f } } }, { { { 13.1f, 14.1f, 15.1f, 16.1f } } } };
 
     ScopedAlignedArrayXVEC output(reinterpret_cast<XVEC*>(_aligned_malloc(sizeof(XVEC) * 8, 16)));
 
     Interleave(reinterpret_cast<XMVECTOR*>(output.get()), reinterpret_cast<const XMVECTOR*>(input), 2, 16 );
 
-    static const XM_ALIGNED_DATA(16) XVEC answer[] =
+    XM_ALIGNED_DATA(16) static const XVEC answer[] =
     {
         { { { 1.f, 1.1f, 2.f, 2.1f } } }, { { { 3.f, 3.1f, 4.f, 4.1f } } }, { { { 5.f, 5.1f, 6.f, 6.1f } } }, { { { 7.f, 7.1f, 8.f, 8.1f } } },
         { { { 9.f, 9.1f, 10.f, 10.1f } } }, { { { 11.f, 11.1f, 12.f, 12.1f } } }, { { { 13.f, 13.1f, 14.f, 14.1f } } }, { { { 15.f, 15.1f, 16.f, 16.1f } } }
@@ -378,7 +378,7 @@ bool FFTInterleavedTest()
 
     FFTInterleaved(reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), reinterpret_cast<XMVECTOR*>(unity.get()), 2, 5 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = {
         { { { 260.799988f, -0.200000f, -0.199998f, -0.200001f } } },
         { { { -32.000000f, -0.199999f, -0.200001f, -0.200000f } } },
         { { { -31.999992f, -0.200000f, -0.200000f, -0.200000f } } },
@@ -396,7 +396,7 @@ bool FFTInterleavedTest()
         { { { -31.999996f, -0.200000f, -0.200000f, -0.200001f } } },
         { { { -32.000004f, -0.200001f, -0.199998f, -0.200001f } } }
     };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = {
         { { { 0.000000f, 2.030633f, 1.005470f, 0.659311f } } },
         { { { 77.254837f, 0.374173f, 0.299321f, 0.243701f } } },
         { { { 31.999996f, 0.164136f, 0.133636f, 0.106902f } } },
@@ -452,7 +452,7 @@ bool IFFTDeinterleavedTest()
 
     IFFTDeinterleaved(reinterpret_cast<XMVECTOR*>(rinput), reinterpret_cast<XMVECTOR*>(iinput), reinterpret_cast<XMVECTOR*>(unity.get()), 2, 5 );
 
-    static const XM_ALIGNED_DATA(16) XVEC ranswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ranswer[] = {
         { { { 8.549999f, 8.750000f, 0.028604f, 0.028604f } } },
         { { { 2.013670f, 2.013670f, 0.007177f, 0.007177f } } },
         { { { 0.707107f, 0.707107f, 0.002721f, 0.002721f } } },
@@ -470,7 +470,7 @@ bool IFFTDeinterleavedTest()
         { { { -1.707107f, -1.707107f, -0.013427f, -0.013427f } } },
         { { { -3.013670f, -3.013670f, -0.034854f, -0.034854f } } }
     };
-    static const XM_ALIGNED_DATA(16) XVEC ianswer[] = {
+    XM_ALIGNED_DATA(16) static const XVEC ianswer[] = {
         { { { 1.4f, 2.4f, 3.4f, 4.4f } } },
         { { { 5.4f, 6.4f, 7.4f, 8.4f } } },
         { { { 9.4f, 10.4f, 11.4f, 12.4f } } },
